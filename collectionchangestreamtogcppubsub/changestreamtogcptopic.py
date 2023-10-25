00cbdbbf5722aa8e0a26620fa75a4f7f390ca8d3
TSowbaranika <109083730+TSowbaranika@users.noreply.github.com>
2023-10-24 19:29:34 -0500

    comments added to the code changestreamtogcptopic.py

--- collectionchangestreamtogcppubsub/changestreamtogcptopic.py
+++ collectionchangestreamtogcppubsub/changestreamtogcptopic.py
@@ -4,7 +4,6 @@ import certifi
 from pymongo import MongoClient
 from google.cloud import pubsub_v1
 from dotenv import load_dotenv
-
 from constants.constants import (
     MONGODB_CONNECTION_URI_STR,
     MONGODB_DATABASE_NAME,
@@ -16,6 +15,7 @@ from constants.constants import (
 load_dotenv()
 class CollectionChangeStreamToGCPTopic:
     def __init__(self):
+        # Initializing the required details from .env file
         self.mongoconnstr = os.getenv(MONGODB_CONNECTION_URI_STR)
         self.dbname = os.getenv(MONGODB_DATABASE_NAME)
         self.collectionname = os.getenv(MONGODB_COLLECTION_NAME)
@@ -23,9 +23,13 @@ class CollectionChangeStreamToGCPTopic:
 
    
     def to_perform_mongo_changestream_to_gcp_topics(self):
+        # Creating connection to the MongoDb with its connection string
         mongo_client = MongoClient(self.mongoconnstr ,tlsCAFile=certifi.where())
+        #Connecting to Database from the Established connection
         db = mongo_client[self.dbname]
+        #Accessing the specific collection in the Database which is connected
         collection = db[self.collectionname]
+        #Watcher to continusously watch all the operations that are happens in the specified collection
         change_stream = collection.watch(full_document="updateLookup")
 
         # Initialize Pub/Sub publisher
